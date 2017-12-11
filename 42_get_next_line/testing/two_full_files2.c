a808bfbe61e96c2195af0bf4271788b99342e3f4
Maksym Haiduk <maksym.haiduk@gmail.com>
2017-12-11 12:43:55 +0000

    tests added

---
+++ 42_get_next_line/testing/two_full_files2.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaiduk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 14:05:58 by mhaiduk           #+#    #+#             */
+/*   Updated: 2017/12/07 17:10:10 by mhaiduk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../get_next_line.h"
+#include <fcntl.h>
+
+#include <stdio.h>
+
+int main(int argc __attribute__ ((unused)), char **argv __attribute__ ((unused)))
+{
+	char *line1 = NULL;
+	char *line2 = NULL;
+    int fd1;
+    int fd2;
+	int res1;
+	int res2;
+
+    fd1 = open(argv[1], O_RDONLY, 0);
+    fd2 = open(argv[2], O_RDONLY, 0);
+
+    while (res1 || res2)
+    {
+        res1 = get_next_line(fd1, &line1);
+        printf("String readed fd: %i : %s\t: with result %i\n", fd1, line1, res1);
+        res2 = get_next_line(fd2, &line2);
+        printf("String readed fd: %i : %s\t: with result %i\n", fd2, line2, res2);
+    }
+
+    ft_strdel(&line1);
+    ft_strdel(&line2);
+	close(fd1);
+	close(fd2);
+	return (0);
+}
