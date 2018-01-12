797889dd740a481ad1707f6bcfcac2e6de371ea2
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-01-12 12:57:12 +0200

    temp

---
+++ 42_fdf/includes/ft_X11.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_X11.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaiduk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/12 09:29:07 by mhaiduk           #+#    #+#             */
+/*   Updated: 2018/01/12 10:10:55 by mhaiduk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_X11_H
+# define FT_X11_H
+
+# define UP_ARROR_KEY		126
+# define DOWN_ARROR_KEY		125
+# define LEFT_ARROR_KEY		123
+# define RIGHT_ARROR_KEY	124
+
+# define PLUS_NUM_KEY		69
+# define MINUS_NUM_KEY		78
+
+#endif
