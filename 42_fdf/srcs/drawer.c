e5bf0df77f220d284d577273667860f7e9d03aba
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-01-16 13:06:27 +0200

    before last check

--- 42_fdf/srcs/drawer.c
+++ 42_fdf/srcs/drawer.c
@@ -6,7 +6,7 @@
 /*   By: mhaiduk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/02 18:29:23 by mhaiduk           #+#    #+#             */
-/*   Updated: 2018/01/16 12:52:10 by mhaiduk          ###   ########.fr       */
+/*   Updated: 2018/01/16 13:03:25 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,13 @@ void		draw_figure(t_env *env)
 			j++;
 		}
 		if (env->obj.vert.values[i][1] == env->obj.vert.values[i + 1][1])
-			if (not_clipped(env->clip.vert.values[i]) ||
+			if (not_clipped(env->clip.vert.values[i]) &&
 				not_clipped(env->clip.vert.values[i + 1]))
 				draw_line(env, i, i + 1);
 		if (i + env->obj.p_in_row[j] < env->obj.vert.rows &&
 			env->obj.vert.values[i][0] ==
 			env->obj.vert.values[i + env->obj.p_in_row[j]][0])
-			if (not_clipped(env->clip.vert.values[i]) ||
+			if (not_clipped(env->clip.vert.values[i]) &&
 				not_clipped(env->clip.vert.values[i + env->obj.p_in_row[j]]))
 				draw_line(env, i, i + env->obj.p_in_row[j]);
 		i++;
