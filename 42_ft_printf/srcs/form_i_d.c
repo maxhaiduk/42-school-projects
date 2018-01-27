055c37db7d03fcf72af1a7db98506b52be991a3c
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-01-27 17:37:51 +0200

    backup

--- 42_ft_printf/srcs/form_i_d.c
+++ 42_ft_printf/srcs/form_i_d.c
@@ -6,7 +6,7 @@
 /*   By: mhaiduk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 14:29:24 by mhaiduk           #+#    #+#             */
-/*   Updated: 2018/01/27 11:20:30 by mhaiduk          ###   ########.fr       */
+/*   Updated: 2018/01/27 17:37:18 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,39 @@ static char	*compute_precision(long long n, char *s, t_fq *fq)
 	return (s);
 }
 
+static char	*add_sign(long long n, char *s, t_fq *fq, size_t *len)
+{
+	if (n < 0)
+		s = fill_right(s, *len + 1, *len, '-');
+	if (fq->flags[PLUS] == '1' && n >= 0)
+		s = fill_right(s, *len + 1, *len, '+');
+	*len = ft_strlen(s);
+	return (s);
+}
+
+static void	set_sign(long long n, char *s, t_fq *fq)
+{
+	if (n < 0)
+		s[0] = '-';
+	if (fq->flags[PLUS] == '1' && n >= 0)
+		s[0] = '+';
+}
+
+static char	*compute_minus(long long n, char *s, t_fq *fq, size_t *len) 
+{		
+	if (fq->flags[MINUS] == '1')
+	{
+		s = add_sign(n, s, fq, len);
+		s = fill_left(s, fq->width, *len, ' ');
+	}
+	else
+	{
+		s = fill_right(s, fq->width, *len, '0');
+		set_sign(n, s, fq);
+	}
+	return (s);
+}
+
 static char	*compute_width(long long n, char *s, t_fq *fq)
 {
 	size_t len;
@@ -36,37 +69,14 @@ static char	*compute_width(long long n, char *s, t_fq *fq)
 		s = compute_precision(n, s, fq);
 	else if (fq->flags[ZERO] == '0')
 	{
-		if (n < 0)
-			s = fill_right(s, len + 1, len, '-');
-		if (fq->flags[PLUS] == '1' && n >= 0)
-			s = fill_right(s, len + 1, len, '+');
-		len = ft_strlen(s);
+		s = add_sign(n, s, fq, &len);
 		if (fq->flags[MINUS] == '1')
 			s = fill_left(s, fq->width, len, ' ');
 		else
 			s = fill_right(s, fq->width, len, ' ');
 	}
 	else if (fq->flags[ZERO] == '1')
-	{
-		len = ft_strlen(s);
-		if (fq->flags[MINUS] == '0')
-		{
-			s = fill_right(s, fq->width, len, '0');
-			if (n < 0)
-				s[0] = '-';
-			if (fq->flags[PLUS] == '1' && n >= 0)
-				s[0] = '+';		
-		}
-		else
-		{
-			if (n < 0)
-				s = fill_right(s, len + 1, len, '-');
-			if (fq->flags[PLUS] == '1' && n >= 0)
-				s = fill_right(s, len + 1, len, '+');
-			len = ft_strlen(s);
-			s = fill_left(s, fq->width, len, ' ');
-		}
-	}
+		s = compute_minus(n, s, fq, &len);
 	return (s);
 }
 
@@ -85,14 +95,36 @@ static char *compute_number(long long n, char *s, t_fq *fq)
 	return (s);
 }
 
+static long long get_value(t_fq *fq, va_list ap)
+{
+	intmax_t n;
+
+    if (fq->size == i)
+        n = (va_arg(ap, int));
+	if (fq->size == hh)
+		n = (char)(va_arg(ap, int));
+	if (fq->size == h)
+		n = (short)(va_arg(ap, int));
+	if (fq->size == ll)
+		n = va_arg(ap, long long);
+	if (fq->size == l)
+		n = (va_arg(ap, long));
+	if (fq->size == j)
+		n = (va_arg(ap, intmax_t));
+	if (fq->size == z)
+		n = (va_arg(ap, size_t));
+	return (n);
+}
+
 void	form_i_d(t_fq *fq, va_list ap)
 {
-	long long	n;
-	char	*s;
-	size_t	len;
+	intmax_t	n;
+	char		*s;
+	size_t		len;
 
-	n = va_arg(ap, int);
-	s = ft_itoa(FT_ABS(n));
+	//n = va_arg(ap, int);
+	n = get_value(fq, ap);
+	s = ft_itoa_abs(n);
 	len = ft_strlen(s);
 	if (fq->precision >= (int)fq->width && fq->precision > (int)len)
 		s = compute_precision(n, s, fq);
@@ -100,8 +132,6 @@ void	form_i_d(t_fq *fq, va_list ap)
 		s = compute_width(n, s, fq);
 	else
 		 s = compute_number(n, s, fq);
-
-
 	fq->str_out = s;
 	fq->str_len = ft_strlen(fq->str_out);
 }
