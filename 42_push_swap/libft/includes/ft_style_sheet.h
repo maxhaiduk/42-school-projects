e46ea95f0d77efa6e6d646b9c684cb02ea409588
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-02-15 13:33:32 +0200

    initial commit

---
+++ 42_push_swap/libft/includes/ft_style_sheet.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_style_sheet.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaiduk <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/08 14:52:11 by mhaiduk           #+#    #+#             */
+/*   Updated: 2018/02/08 14:52:12 by mhaiduk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STYLE_SHEET_H
+# define FT_STYLE_SHEET_H
+
+# define RESET      "\x1b[0m"
+
+# define BOLD       "\x1b[1m"
+# define UNDERLINE  "\x1b[4m"
+# define INVERT     "\x1b[7m"
+
+# define BLACK      "\x1b[30m"
+# define RED        "\x1b[31m"
+# define GREEN      "\x1b[32m"
+# define YELLOW     "\x1b[33m"
+# define BLUE       "\x1b[34m"
+# define MAGENTA    "\x1b[35m"
+# define CYAN       "\x1b[36m"
+# define WHITE      "\x1b[37m"
+# define F_RESET    "\x1b[39m"
+
+# define B_BLACK    "\x1b[40m"
+# define B_RED      "\x1b[41m"
+# define B_GREEN    "\x1b[42m"
+# define B_YELLOW   "\x1b[43m"
+# define B_BLUE     "\x1b[44m"
+# define B_MAGENTA  "\x1b[45m"
+# define B_CYAN     "\x1b[46m"
+# define B_WHITE    "\x1b[47m"
+# define B_RESET    "\x1b[49m"
+
+#endif
