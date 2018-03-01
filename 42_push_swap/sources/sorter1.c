b3c37bc220bea976de5062387352551641b9a660
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-03-01 14:18:36 +0200

    primal division without optimization done

--- 42_push_swap/sources/sorter1.c
+++ 42_push_swap/sources/sorter1.c
@@ -6,7 +6,7 @@
 /*   By: mhaiduk <mhaiduk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 18:40:30 by mhaiduk           #+#    #+#             */
-/*   Updated: 2018/03/01 14:01:10 by mhaiduk          ###   ########.fr       */
+/*   Updated: 2018/03/01 14:15:50 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@ void	primal_dividing(t_list **a, t_list **b, t_list **tail_a)
 {
 	int len;
 	int med;
-	int x;
-	int y;
-	int z;
 
 	len = (int)ft_lstlen(*a);
 	len = len % 2 ? len / 2 + 1 : len / 2;
@@ -36,24 +33,14 @@ void	primal_dividing(t_list **a, t_list **b, t_list **tail_a)
 	ft_printf("Mediana is %i\n", med);
 	while (len)
 	{
-		if (*a && (*a)->next)
+		(*a)->content_size = med;
+		ft_printf("NODE: content: %i\ncontent size: %i\n",*((int *)(*a)->content), (*a)->content_size = med);
+		if (VAL(a) > med)
+			ra(a, tail_a);
+		else
 		{
-			x = VAL(a);
-			y = VAL(tail_a);
-			z = VAL_NEXT(a);
-			if (x <= med && is_min(x, y, z))
-				;
-			else if (z <= med && is_min(z, y, x))
-				sa(*a);
-			else if (y <= med && is_min(y, x, z))
-				rra(a, tail_a);
-			else
-				ra(a, tail_a);
-			if (VAL(a) <= med)
-			{
-				pb(a, b);
-				len--;
-			}
+			pb(a, b);
+			len--;
 		}
 		print_stacks(*a, *b);
 	}
