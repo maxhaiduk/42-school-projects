471c609a163662c038d633578ac28663ddc81930
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-03-06 15:31:01 +0200

    acceptable efficiency

--- 42_push_swap/sources/sort_group_a.c
+++ 42_push_swap/sources/sort_group_a.c
@@ -6,7 +6,7 @@
 /*   By: mhaiduk <mhaiduk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 11:50:54 by mhaiduk           #+#    #+#             */
-/*   Updated: 2018/03/06 12:38:49 by mhaiduk          ###   ########.fr       */
+/*   Updated: 2018/03/06 13:33:16 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static void    sort_vol3(t_list **a, t_list **b)
 {
+    (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
+    ra(a);
     sa(*a);
-    pb(a, b);
-    sa(*a);
-    pa(a, b);
+    rra(a);
     sa(*a); 
 }
 static void    sort_vol2(t_list **a, t_list **b)
@@ -32,17 +32,17 @@ static void    sort_vol2(t_list **a, t_list **b)
 
     if (P_120(top, next, bot))
     {   
-        pb(a, b);
-        sa(*a);
-        pa(a, b);
+        ra(a);
+        (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
+        rra(a);
         sa(*a);
     }
     else if (P_201(top, next, bot))
     {
+        (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
+        ra(a);
         sa(*a);
-        pb(a, b);
-        sa(*a);
-        pa(a, b);
+        rra(a);
     }
     else if (P_210(top, next, bot))
         sort_vol3(a, b);
@@ -59,12 +59,12 @@ static void    sort_3_nums(t_list **a, t_list **b)
     bot = VAL_N_NEXT(a);
     if (P_021(top, next, bot))
     {
-        pb(a, b);
-        sa(*a);
-        pa(a, b);
+        ra(a);
+        (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
+        rra(a);
     }
     else if (P_102(top, next, bot))
-        sa(*a);
+        (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
     else
         sort_vol2(a, b);
 }
@@ -91,12 +91,7 @@ void       sort_group_a(t_list **a, t_list **b)
     if (stack_sorted_a(*a, n))
         ;
     else if (n == 2)
-    {
-        if (*b && (*b)-> next && VAL(b) < VAL_NEXT(b))
-            ss(*a, *b);
-        else
-            sa(*a);
-    }
+        (*b && (*b)-> next && VAL(b) < VAL_NEXT(b)) ? ss(*a, *b) : sa(*a);
     else if (n == 3)
     {
         if (one_group_in_stack(a))
