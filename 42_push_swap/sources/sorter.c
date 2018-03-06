4ced36ec0164b3c6c563e0883d78882c0c3a88e0
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-03-06 12:44:37 +0200

    checking for sorted stack done

--- 42_push_swap/sources/sorter.c
+++ 42_push_swap/sources/sorter.c
@@ -6,7 +6,7 @@
 /*   By: mhaiduk <mhaiduk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 18:40:30 by mhaiduk           #+#    #+#             */
-/*   Updated: 2018/03/05 14:11:51 by mhaiduk          ###   ########.fr       */
+/*   Updated: 2018/03/06 12:43:39 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ int      one_group_in_stack(t_list **a)
 
 void	sort_stack(t_list **a, t_list **b)
 {
+	if (stack_sorted_a(*a, get_stack_len(*a)))
+		return ;
 	compute_a_stack(a, b);
 	compute_b_stack(a, b);
 }
