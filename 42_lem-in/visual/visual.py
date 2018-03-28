55ce8457cfdfcaffbaa2108c94ad4b298b6c5283
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-03-28 19:47:02 +0300

    new reading

--- 42_lem-in/visual/visual.py
+++ 42_lem-in/visual/visual.py
@@ -3,33 +3,57 @@ import sys
 import re
 import drawer as dr
 
+
+
 def read_data():
 	rooms = {}
 	links = []
+	moves = []
+
 	for line in sys.stdin:
 		line = line.rstrip('\n')
-		if (line == "##start\n"):
-			line = sys.stdin.next()
+
+		if (re.match('^\d+$', line)):
+			ants = int(line)
+		#Execute ##start instruction
+		elif (line == "##start"):
+			line = sys.stdin.next().strip('\n')
+			name, x, y = line.split(' ')
+			rooms[name] = [int(x), int(y), 's']
+			continue
+		
+		#Execute ##end instruction
+		elif (line == "##end"):
+			line = sys.stdin.next().strip('\n')
+			name, x, y = line.split(' ')
+			rooms[name] = [int(x), int(y), 'e']
 			continue
 
 		#Room parameter (smth like A 2 3)
-		if (re.match('\w+ \d+ \d+', line)):
-			temp = line.split(' ')
-			rooms[temp[0]] = [int(temp[1]), int(temp[2])]
+		elif (re.match('\w+ \d+ \d+', line)):
+			name, x, y = line.split(' ')
+			rooms[name] = [int(x), int(y), 'c']
 		
 		#Link between rooms (smth like A-B)
 		elif (re.match('^(?!L)(\w|\d)+-(\w|\d)+$', line)):
 			temp = line.split('-')
 			links.append((temp[0], temp[1]))
+
+		elif (re.match('^L+', line)):
+			moves.append(line)
 		print (line)
-	return(rooms, links)
+	return(ants, rooms, links, moves)
 
 def main():
-	rooms, links = read_data()
-	if (len(rooms) == 0):
-		print ("Error")
-		exit (1)
-	dr.draw_graph(rooms, links)
+	ants, rooms, links, moves = read_data()
+	# if (len(rooms) == 0):
+	# 	print ("Error")
+	# 	exit (1)
+	# dr.draw_graph(rooms, links)
+	print (ants)
+	print (rooms)
+	print (links)
+	print (moves)
 
 if __name__ == "__main__":
 	main()
