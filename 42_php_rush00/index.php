978e6ae4f53fc2b16f26bfbe67408588f53388c9
Maksym Haiduk <maksym.haiduk@gmail.com>
2018-04-08 16:27:14 +0300

    styles added

--- 42_php_rush00/index.php
+++ 42_php_rush00/index.php
@@ -10,42 +10,28 @@
 
 			<div class="header">
 				<div class="header_content">
-						<p>MUSIC SHOW</p>
+						<p>MUSIC SHOP</p>
 						<div></div>
 						<?php
 							session_start();
 							if (isset($_SESSION['login']))
-								echo '<p><a href="log_out.php">Log_out</a></p>';
+								echo '<a href="log_out.php"><button type="button" class="log_in">Logout</button></a>';
 							else
-								echo '<p><a href="log_in.php">Log_in</a></p>';
+								echo '<a href="log_in.php"><button type="button" class="log_in">Login</button></a>';
 						?>
-						<p>Basket</p>
+						<a href="log_in.html"><button type="button" class="basket">BASKET</button></a>
 				</div>
 			</div>
-
 				<div class="sidebar">
-					<div class="sidebar">
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00/index.php?category=all">All</a>
-						</button>
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00/index.php?category=drums">drums</a>
-						</button>
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00?category=eguitars">electric guitars</a>
-						</button>
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00?category=aguitars">acoustic guitars</a>
-						</button>
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00?category=synt">synthesizer</a>
-						</button>
-						<button class="category1">
-							<a href="http://localhost:8100/RUSH00?category=sax">saxophones</a>
-						</button>
+					<div class="sidebar">	
+							<a href="http://localhost:8100/RUSH00?category=all"><button class="category1">All</button></a>
+							<a href="http://localhost:8100/RUSH00?category=drums"><button class="category1">drums</button></a>
+							<a href="http://localhost:8100/RUSH00?category=eguitars"><button class="category1">electric guitars</button></a>
+							<a href="http://localhost:8100/RUSH00?category=aguitars"><button class="category1">acoustic guitars</button></a>
+							<a href="http://localhost:8100/RUSH00?category=synt"><button class="category1">synthesizer</button></a>
+							<a href="http://localhost:8100/RUSH00?category=sax"><button class="category1">saxophones</button></a>
 					</div>
 				</div>
-	
 			<div class="content">
 			<?php
 				require_once('display_content.php');
