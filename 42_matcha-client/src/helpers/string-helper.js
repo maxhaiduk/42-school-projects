b72320ae417df3630e8d8f65f0922ffd466a6c03
Maksym Haiduk <maksym.haiduk@gmail.com>
2019-07-15 15:27:17 +0300

    Supplemented StringHelper

--- 42_matcha-client/src/helpers/string-helper.js
+++ 42_matcha-client/src/helpers/string-helper.js
@@ -1,12 +1,58 @@
 export default class StringHelper {
 
     /**
-     * Capitalises firs character of the string
+     * Capitalises first character of the string
      *
      * @param {string} string
      * @return {string}
      */
     static ucFirst(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    /**
+     * Capitalises first character of each word in the string
+     *
+     * @param string
+     * @return {string}
+     *
+     * 'foo bar' => 'Foo Bar'
+     * 'fOO bAr' => 'FOO BAr'
+     */
+    static capitalize(string) {
+        return string.split(' ')
+            .map((word) => this.ucFirst(word))
+            .join(' ');
+    }
+
+    /**
+     * Converts string to humanCase
+     *
+     * @param string
+     * @return {string}
+     *
+     * 'camelCase' => 'camel Case'
+     * 'kebab-case' => 'kebab case'
+     * 'snake_case' => 'snake case'
+     */
+    static toHumanCase(string) {
+        return string.replace(this._getCommonPattern(), ' ');
+    }
+
+    /**
+     * Converts string to humanCase and capitalizes each word
+     * @see toHumanCase
+     *
+     * @param string
+     * @return {string}
+     *
+     * 'kebab-case' => 'Kebab Case'
+     */
+    static toHumanCaseCap(string) {
+        return this.capitalize(this.toHumanCase(string));
+    }
+
+    static _getCommonPattern() {
+        return /(?<=[a-z])(?=[A-Z])|_| |-/;
     }
 }
