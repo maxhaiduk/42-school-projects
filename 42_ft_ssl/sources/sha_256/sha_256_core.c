d6c7e2bb5cdb532f66c41e2484954f90614406b0
Maksym Haiduk <maksym.haiduk@gmail.com>
2019-08-10 17:39:14 +0300

    Started sha-256

--- 42_ft_ssl/sources/sha_256/sha_256_core.c
+++ 42_ft_ssl/sources/sha_256/sha_256_core.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hash_string.c                                      :+:      :+:    :+:   */
+/*   sha_256_core.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhaiduk <mhaiduk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/10 11:41:32 by mhaiduk           #+#    #+#             */
-/*   Updated: 2019/08/10 13:58:40 by mhaiduk          ###   ########.fr       */
+/*   Created: 2019/08/10 17:26:10 by mhaiduk           #+#    #+#             */
+/*   Updated: 2019/08/10 17:29:38 by mhaiduk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_md5.h"
+#include "ft_sha_256.h"
 
-void	md5_hash_string(char *string)
+void	md5_init(t_sha_256_context *context)
 {
-	t_md5_context	context;
-	unsigned char	digest[MD5_DIGEST_SIZE];
-	size_t			length;
-
-	length = ft_strlen(string);
-	md5_init(&context);
-	md5_update(&context, (unsigned char *)string, length);
-	md5_finish(digest, &context);
-	md5_print(digest, string, FT_FALSE);
+	context->source_size_bits = 0;
+	context->state[0] = 0x6A09E667;
+	context->state[1] = 0xBB67AE85;
+	context->state[2] = 0x3C6EF372;
+	context->state[3] = 0xA54FF53A;
+	context->state[4] = 0x510E527F;
+	context->state[5] = 0x9B05688C;
+	context->state[6] = 0x1F83D9AB;
+	context->state[7] = 0x5BE0CD19;
 }
---
+++ 42_ft_ssl/sources/sha_256/sha_256_hash_string.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sha_256_hash_string.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaiduk <mhaiduk@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/10 17:22:47 by mhaiduk           #+#    #+#             */
+/*   Updated: 2019/08/10 17:30:36 by mhaiduk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_sha_256.h"
+
+void	sha_256_hash_string(char *string)
+{
+	t_sha_256_context	context;
+	unsigned char		digest[SHA_256_DIGEST_SIZE];
+	size_t				length;
+
+	length = ft_strlen(string);
+	sha_256_init(&context);
+	sha_256_update(&context, (unsigned char *)string, length);
+	sha_256_finish(digest, &context);
+	sha_256_print(digest, string, FT_FALSE);
+}
