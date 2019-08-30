4d731e93e02ee796960a766b7feda902d3ebc991
Maksym Haiduk <maksym.haiduk@gmail.com>
2019-08-30 16:20:10 +0300

    removed memory resetter

--- 42_ft_malloc/sources/memory.c
+++ 42_ft_malloc/sources/memory.c
@@ -6,7 +6,7 @@
 /*   By: maks <maksym.haiduk@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 16:02:47 by maks              #+#    #+#             */
-/*   Updated: 2019/08/30 16:17:42 by maks             ###   ########.fr       */
+/*   Updated: 2019/08/30 16:19:42 by maks             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,6 @@ void		*allocate_memory(size_t size)
 	return (mem);
 }
 
-static void	unallocate_zone(t_memory_zone *zone)
-{
-	t_block_header	*header;
-	t_block_header	*start;
-	t_block_header	*prev;
-	size_t			size;
-
-	size = 0;
-	header = zone->first_block;
-	start = header;
-	prev = header;
-	while ((header = header->next))
-	{
-		size += REAL_BLOCK_SIZE(prev);
-		if (!header->next)
-		{
-			munmap(start, ALIGN_TO_PAGE_SIZE(size + REAL_BLOCK_SIZE(header)));
-			return ;
-		}
-		else if (!BLOCKS_CONTINIOUS(prev, header))
-		{
-			munmap(start, ALIGN_TO_PAGE_SIZE(size));
-			start = header;
-			size = 0;
-		}
-		prev = header;
-	}
-}
-
 void		reset_allocations(void)
 {
 	t_memory_zone	*zone;
@@ -60,8 +31,6 @@ void		reset_allocations(void)
 	while (i < ZONE_QTY)
 	{
 		zone = &g_memory_zones[i];
-		if (zone->first_block)
-			unallocate_zone(zone);
 		zone->first_block = NULL;
 		zone->size = 0;
 		i++;
