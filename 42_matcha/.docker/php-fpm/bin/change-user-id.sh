9abf4cf3ac7fe7ee61b00d96c5e26f7bde732850
Maksym Haiduk <maksym.haiduk@gmail.com>
2020-01-01 13:35:38 +0200

    Refactore php-fpm docker scripts

--- 42_matcha/.docker/php-fpm/bin/change-user-id.sh
+++ 42_matcha/.docker/php-fpm/bin/change-user-id.sh
@@ -6,19 +6,19 @@ USER=$1
 NEW_UID=$2
 NEW_GID=$3
 
-MY_UID=$(id -u $USER)
-MY_GID=$(id -g $USER)
+MY_UID=$(id -u "$USER")
+MY_GID=$(id -g "$USER")
 
 function change_uid ()
 {
-    OLD=$1; NEW=$2; usermod -u $NEW $(id -un $OLD)
-    find / -xdev -user $OLD -exec chown -h $NEW {} \;
+    OLD=$1; NEW=$2; usermod -u "$NEW" "$(id -un "$OLD")"
+    find / -xdev -user "$OLD" -exec chown -h "$NEW" {} \;
 }
 
 function change_gid ()
 {
-    OLD=$1; NEW=$2; groupmod -g $NEW $(getent group $OLD | cut -d: -f1)
-    find / -xdev -group $OLD -exec chgrp -h $NEW {} \;
+    OLD=$1; NEW=$2; groupmod -g "$NEW" "$(getent group "$OLD" | cut -d: -f1)"
+    find / -xdev -group "$OLD" -exec chgrp -h "$NEW" {} \;
 }
 
 function nextent ()
@@ -26,34 +26,34 @@ function nextent ()
     TYPE=$1; EXCEPT=$2; START=1000; RESULT=$3
 
     while true; do
-        if [ $START -eq $EXCEPT ]; then
+        if [ "$START" -eq "$EXCEPT" ]; then
             START=$((START + 1)); continue
         fi
 
-        if getent $TYPE $START > /dev/null; then
+        if getent "$TYPE" "$START" > /dev/null; then
             START=$((START + 1)); continue
         fi
 
         break
     done
 
-    eval $RESULT=$START
+    eval "$RESULT"="$START"
 }
 
 if [ "$MY_UID" -ne "$NEW_UID" ]; then
-    if getent passwd $NEW_UID > /dev/null; then
-        nextent 'passwd' $NEW_UID FREE_UID
-        change_uid $NEW_UID $FREE_UID
+    if getent passwd "$NEW_UID" > /dev/null; then
+        nextent 'passwd' "$NEW_UID" FREE_UID
+        change_uid "$NEW_UID" "$FREE_UID"
     fi
     echo "Changing UID $MY_UID to $NEW_UID"
-    change_uid $MY_UID $NEW_UID
+    change_uid "$MY_UID" "$NEW_UID"
 fi
 
 if [ "$MY_GID" -ne "$NEW_GID" ]; then
-    if getent group $NEW_GID > /dev/null; then
-        nextent 'group' $NEW_GID FREE_GID
-        change_gid $NEW_GID $FREE_GID
+    if getent group "$NEW_GID" > /dev/null; then
+        nextent 'group' "$NEW_GID" FREE_GID
+        change_gid "$NEW_GID" "$FREE_GID"
     fi
     echo "Changing GID $MY_GID to $NEW_GID"
-    change_gid $MY_GID $NEW_GID
+    change_gid "$MY_GID" "$NEW_GID"
 fi
